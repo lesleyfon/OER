cd14c2e9465efdf148c871782bd808030b39c2c7
lesley <lesleybanadzem2017@gmail.com>
2019-11-14 13:02:06 -0800

    Created store for books and created a reducer funtion

--- oer/src/index.js
+++ oer/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import { Provider, } from 'react-redux';
+
+import {reducer as bookReducer} from './reducer/allBooks'
 import App from './App';
 
+const allReducers = combineReducers({bookReducer});
+
+const store = createStore(allReducers)
 ReactDOM.render(
-    <Router>
+    <Provider store= {store}>
+        <Router>
         <App />
-    </Router>,
+    </Router>    
+    </Provider>,
     document.getElementById('root')
     );
---
+++ oer/src/reducer/allBooks.js
@@ -0,0 +1,32 @@
+const initialState = {
+    fetching : false,
+    books : [],
+    error : ''
+}
+export const FETCHING_BOOK_DATA = 'FETCHING_BOOK_DATA';
+export const FETCHING_BOOK_SUCCESS = 'FETCHING_BOOK_SUCCESS';
+export const FETCHING_BOOK_ERROR = 'FETCHING_BOOK_ERROR';
+
+export function reducer (state = initialState, action) {
+    switch(action.type){
+        case FETCHING_BOOK_DATA:
+            return {
+                ...state,
+                fetching: true
+            }
+        case FETCHING_BOOK_SUCCESS:
+            return {
+                ...state,
+                fetching: false,
+                books : action.payload
+            }
+        case FETCHING_BOOK_ERROR:
+            return {
+                ...state,
+                fetching: false,
+                error : action.payload
+            }
+        default:
+            return state
+    }
+}
