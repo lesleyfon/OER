fb532a67b02c1879ed7878d25a200fb33b2985fe
lesley <lesleybanadzem2017@gmail.com>
2019-11-14 14:24:27 -0800

    Styled Header

--- oer/src/Components/Header/NavBar.js
+++ oer/src/Components/Header/NavBar.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 function NavBar(props) {
-
+    const [search, setSearch] = useState('')
     const logOut = e => {
         e.preventDefault();
         localStorage.removeItem('token');
         props.history.push('/')
+    }
+    const handleSearch = e => {
+        setSearch(e.target.value)
     }
     return (
         <NavBarStyled>
@@ -19,8 +22,13 @@ function NavBar(props) {
                 </ul>
             </nav>
 
-            <div>
-                <input />
+            <div className='right-container'>
+                <input 
+                    placeholder='Search'
+                    name= 'search'
+                    value= {search}
+                    onChange={handleSearch}
+                />
                 <button onClick= {logOut}>LogOut</button>
             </div>
         </NavBarStyled>
@@ -37,4 +45,53 @@ const NavBarStyled = styled.div`
     right: 0px;
     height: 57px;
     background-color: #fc4503;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+    nav ul{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+        width: 400px;
+        margin: 0px;
+    }
+    nav ul li{
+        list-style-type: none;
+        padding: 10px 20px;
+        font-weight: bolder;
+        border-radius: 10px;
+        a{
+            text-decoration: none;
+            color: #000000
+        }
+    }
+    nav ul li:hover{
+        background-color: #FAFBDB;
+        transition-duration: .9s;
+    }
+
+    .right-container{
+        justify-content: space-evenly;
+        display: flex;
+        align-content: center;
+        padding-right: 10px;
+        width: 300px;
+    }
+    .right-container input{
+        font-size: 1rem;
+        padding-left: 10px
+
+    }
+    .right-container button{
+        padding: 10px 20px;
+        font-size: 1rem;
+        font-weight: 132px;
+        height: 75%;
+        align-self: center;
+        text-align: center;
+        background: #FAFBDB;
+        border-radius: 10px;
+
+    }
 `;
