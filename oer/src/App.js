d04ed193e28dc430005d09cfbd54d34fe8b6fb26
lesley <lesleybanadzem2017@gmail.com>
2019-11-13 17:30:37 -0800

    Initial commit

---
+++ oer/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        hello OER
+      </header>
+    </div>
+  );
+}
+
+export default App;
